885684cb85e42f10a8a825f94871a2dce7b7df57
Hugues, programmer fullstack <docteurfutur@gmail.com>
2024-09-13 11:46:40 +0200

    fetch JSON datas & create photographer link

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,9 +1,7 @@
-/*     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes 
-        //de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON 
-        //en utilisant "fetch".
-        /* let photographers = [
+/* async function getPhotographers() {
+        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+    let photographers = [
             {
                 "name": "Ma data test",
                 "id": 1,
@@ -22,21 +20,56 @@
                 "price": 500,
                 "portrait": "account.png"
             },
-        ] */
+        ]
         // et bien retourner le tableau photographers seulement une fois récupéré
-       
-        // suivant les indications, avec l'instanciation de l'objet 'PhotographerApi', on accède à la méthode 'fetch()' pr requêterles données sur le fichier JSON       
- /*       
-        const photographers = new PhotographerApi('./../data/photographers.json');
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    } 
-*/
+            photographers: [...photographers, ...photographers, ...photographers]}
+        )
+    } */
 
+/**
+ * fetch photographers datas
+ */
+
+
+async function getPhotographers() {
+    const response = await fetch('./data/photographers.json')
+    const data = await response.json()
+        
+    //console.log(data); 
+    return data.photographers;   
+}
+
+async function displayData(photographers) {
+    console.log(photographers)
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const photographers = await getPhotographers();
+    console.log(photographers);
+    
+    displayData(photographers);
+} 
+    
+init();
+
+
+/**************************************************************************************
+ * 
+ **********************************************************************************************/
+/* 
 class App {
     constructor() {
         this.photographersSection = document.querySelector(".photographer_section");
-        this.photographersApi = new PhotographerApi('../../data/photographers.json');
+        this.photographersApi = new PhotographerApi('./../data/photographers.json');
     }
 
     async main() {
@@ -46,31 +79,16 @@ class App {
     
         photographers
             .map(photographer => {
-                //new Photographer(photographer);
+                new Photographer(photographer);
                 console.log(photographer);
             })  
             
     }   
 }
-    /* async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    } */
+    
 
 const app = new App();
 app.main();
+ */
 
-/*     async function init() {
-        // Récupère les datas des photographes
-        const photographersApi = new PhotographerApi('./../data/photographers.json');
-        const { photographers } = await photographersApi.getPhotographers();
-        displayData(photographers);
-    } */
-    
-    // init();
     
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,23 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        const link = document.createElement( 'a' );
+        link.href = '/Front-End-Fisheye/photographer.html?id=' + id;
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        link.appendChild(article);
+        return (link);
     }
-    return { name, picture, getUserCardDOM }
+
+    function getUserHeaderDOM() {}
+    
+    return { name, picture, getUserCardDOM, getUserHeaderDOM }
 }
